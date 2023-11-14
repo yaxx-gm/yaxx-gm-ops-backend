6ae084d802a4ed660f6f70191e77327096d01903
Pablo Magalhães <109962972+pabloaugustocm17@users.noreply.github.com>
2023-11-13 21:19:08 -0300

    feat: user repository

---
+++ yaxx/src/main/java/org/br/yaxx/backend/repositories/UserRepository.java
@@ -0,0 +1,22 @@
+package org.br.yaxx.backend.repositories;
+
+import org.br.yaxx.backend.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    @Query(
+            "SELECT U.id " +
+            "FROM User AS U " +
+            "WHERE U.mail = :mail"
+    )
+    Optional<User> getUserByMail(@Param("mail") String mail);
+
+}
