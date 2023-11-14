18d81bd8f7a52f63f1e59f30ac4e89384442c8a3
Pablo Magalhães <109962972+pabloaugustocm17@users.noreply.github.com>
2023-11-13 21:16:56 -0300

    chore: first commit

---
+++ yaxx/src/test/java/org/br/yaxx/backend/YaxxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.br.yaxx.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YaxxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
